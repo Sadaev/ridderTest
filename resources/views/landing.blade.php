b671d160b0bb9e8d207ef7622829b581395e2f51
Roman <orehov_roman@mail.ru>
2021-07-16 14:46:28 +0600

    Title: Меняем заголовок страницы на "Вакцинация в г.Риддер"

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Вакцинация в г.Риддер</title>
     <link rel="stylesheet" href="{{asset('css/mainStyle.css')}}">
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <script src="{{asset('js/app.js')}}"></script>
