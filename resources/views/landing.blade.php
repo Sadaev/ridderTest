782da1144ac73265b67480eb7297b3f6d2065242
Швнгвс Садаев <sadaevsh@gmail.com>
2021-08-04 15:42:00 +0600

    Фикс с иконкой

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,6 +13,7 @@
     @livewireStyles
 </head>
 <body>
+
 <svg style="display: none;" xmlns="http://www.w3.org/2000/svg">
     <symbol id="LOGO" viewBox="0 0 512 512" enable-background="new 0 0 512 512">
         <g>
@@ -66,13 +67,13 @@
             </g>
         </g>
     </symbol>
-    <symbol id="instagram" viewBox="0 0 24 24" enable-background="new 0 0 24 24">
-        <linearGradient id="SVGID_1_" gradientTransform="matrix(0 -1.982 -1.844 0 -132.522 -51.077)" gradientUnits="userSpaceOnUse" x1="-37.106" x2="-26.555" y1="-72.705" y2="-84.047"><stop offset="0" stop-color="#fd5"/>
-            <stop offset=".5" stop-color="#ff543e"/>
-            <stop offset="1" stop-color="#c837ab"/>
-        </linearGradient><path d="m1.5 1.633c-1.886 1.959-1.5 4.04-1.5 10.362 0 5.25-.916 10.513 3.878 11.752 1.497.385 14.761.385 16.256-.002 1.996-.515 3.62-2.134 3.842-4.957.031-.394.031-13.185-.001-13.587-.236-3.007-2.087-4.74-4.526-5.091-.559-.081-.671-.105-3.539-.11-10.173.005-12.403-.448-14.41 1.633z" fill="url(#SVGID_1_)"/>
-        <path d="m11.998 3.139c-3.631 0-7.079-.323-8.396 3.057-.544 1.396-.465 3.209-.465 5.805 0 2.278-.073 4.419.465 5.804 1.314 3.382 4.79 3.058 8.394 3.058 3.477 0 7.062.362 8.395-3.058.545-1.41.465-3.196.465-5.804 0-3.462.191-5.697-1.488-7.375-1.7-1.7-3.999-1.487-7.374-1.487zm-.794 1.597c7.574-.012 8.538-.854 8.006 10.843-.189 4.137-3.339 3.683-7.211 3.683-7.06 0-7.263-.202-7.263-7.265 0-7.145.56-7.257 6.468-7.263zm5.524 1.471c-.587 0-1.063.476-1.063 1.063s.476 1.063 1.063 1.063 1.063-.476 1.063-1.063-.476-1.063-1.063-1.063zm-4.73 1.243c-2.513 0-4.55 2.038-4.55 4.551s2.037 4.55 4.55 4.55 4.549-2.037 4.549-4.55-2.036-4.551-4.549-4.551zm0 1.597c3.905 0 3.91 5.908 0 5.908-3.904 0-3.91-5.908 0-5.908z" fill="#fff"/>
-    </symbol>
+{{--    <symbol id="instagram" viewBox="0 0 24 24" enable-background="new 0 0 24 24">--}}
+{{--        <linearGradient id="SVGID_1_" gradientTransform="matrix(0 -1.982 -1.844 0 -132.522 -51.077)" gradientUnits="userSpaceOnUse" x1="-37.106" x2="-26.555" y1="-72.705" y2="-84.047"><stop offset="0" stop-color="#fd5"/>--}}
+{{--            <stop offset=".5" stop-color="#ff543e"/>--}}
+{{--            <stop offset="1" stop-color="#c837ab"/>--}}
+{{--        </linearGradient><path d="m1.5 1.633c-1.886 1.959-1.5 4.04-1.5 10.362 0 5.25-.916 10.513 3.878 11.752 1.497.385 14.761.385 16.256-.002 1.996-.515 3.62-2.134 3.842-4.957.031-.394.031-13.185-.001-13.587-.236-3.007-2.087-4.74-4.526-5.091-.559-.081-.671-.105-3.539-.11-10.173.005-12.403-.448-14.41 1.633z" fill="url(#SVGID_1_)"/>--}}
+{{--        <path d="m11.998 3.139c-3.631 0-7.079-.323-8.396 3.057-.544 1.396-.465 3.209-.465 5.805 0 2.278-.073 4.419.465 5.804 1.314 3.382 4.79 3.058 8.394 3.058 3.477 0 7.062.362 8.395-3.058.545-1.41.465-3.196.465-5.804 0-3.462.191-5.697-1.488-7.375-1.7-1.7-3.999-1.487-7.374-1.487zm-.794 1.597c7.574-.012 8.538-.854 8.006 10.843-.189 4.137-3.339 3.683-7.211 3.683-7.06 0-7.263-.202-7.263-7.265 0-7.145.56-7.257 6.468-7.263zm5.524 1.471c-.587 0-1.063.476-1.063 1.063s.476 1.063 1.063 1.063 1.063-.476 1.063-1.063-.476-1.063-1.063-1.063zm-4.73 1.243c-2.513 0-4.55 2.038-4.55 4.551s2.037 4.55 4.55 4.55 4.549-2.037 4.549-4.55-2.036-4.551-4.549-4.551zm0 1.597c3.905 0 3.91 5.908 0 5.908-3.904 0-3.91-5.908 0-5.908z" fill="#fff"/>--}}
+{{--    </symbol>--}}
     <symbol id="vk" x="0px" y="0px" viewBox="0 0 112.196 112.196" style="enable-background:new 0 0 112.196 112.196;" xml:space="preserve">
             <g>
                 <g>
@@ -101,9 +102,7 @@
                 C46.496,446.88,101.6,464,161.024,464c193.152,0,298.752-160,298.752-298.688c0-4.64-0.16-9.12-0.384-13.568
                 C480.224,136.96,497.728,118.496,512,97.248z"/>
 	</symbol>
-
-
-<symbol id="facebook" x="0px" y="0px"
+    <symbol id="facebook" x="0px" y="0px"
 	 viewBox="0 0 512.002 512.002" style="enable-background:new 0 0 512.002 512.002;" xml:space="preserve">
 <circle style="fill:#4E598F;" cx="256.001" cy="256" r="256"/>
 <path style="fill:#364270;" d="M511.596,241.7L391.019,121.085c-1.998,0.605-6.982-1.714-9.173-1.274
@@ -124,8 +123,7 @@
 	h40.351c3.848,0,6.83,3.358,6.375,7.173l-4.001,33.759c-0.381,3.232-3.122,5.665-6.375,5.665h-36.168l-0.183,104.726h62.826
 	c21.809,0,39.49-17.682,39.49-39.491V148.956C402.533,127.147,384.852,109.466,363.043,109.466z"/>
 </symbol>
-
-<symbol id="ok" x="0px" y="0px"
+    <symbol id="ok" x="0px" y="0px"
 	 viewBox="0 0 97.75 97.75" style="enable-background:new 0 0 97.75 97.75;" xml:space="preserve"
 	>
 <g>
@@ -145,8 +143,8 @@
 			c0.754-0.576,1.561-1.05,2.496-1.289c1.816-0.468,3.512,0.201,4.486,1.791C69.613,52.874,69.6,54.646,68.227,56.057z"/>
 	</g>
 </symbol>
-
 </svg>
+
 <main class="container">
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -428,31 +426,31 @@
                 </div>
                 <div class="boxContainer d-flex justify-content-between">
                     <div class="itemSocial">
-			<div class="socialIcon">
-			    <a href="https://instagram.com/akimatridder" target="_blank">
-	                            <svg class="bi me-2" width="32" height="32"><use xlink:href="#instagram"/></svg>
-			    </a>
-                        </div>
-                    </div>
-                    <div class="itemSocial">
-                        <div class="socialIcon">
-			    <a href="https://www.facebook.com/profile.php?id=100009764318178" target="_blank">
-                            	<svg class="bi me-2" width="32" height="32"><use xlink:href="#facebook"/></svg>
-			    </a>
-                        </div>
-                    </div>
-                    <div class="itemSocial">
-                        <div class="socialIcon">
-			    <a href="https://ok.ru/profile/588834059008" target="_blank">
-                            	<svg class="bi me-2" width="32" height="32"><use xlink:href="#ok"/></svg>
-			    </a>
-                        </div>
-                    </div>
+                <div class="socialIcon">
+                    <a href="https://instagram.com/akimatridder" target="_blank">
+                        <img src="{{asset('images/instagram.png')}}" alt="Иконка инстаграмма" style="display: block">
+                    </a>
+                </div>
+            </div>
+            <div class="itemSocial">
+                <div class="socialIcon">
+                    <a href="https://www.facebook.com/profile.php?id=100009764318178" target="_blank">
+                        <svg class="bi me-2" width="32" height="32"><use xlink:href="#facebook"/></svg>
+                    </a>
+                </div>
+            </div>
+            <div class="itemSocial">
+                <div class="socialIcon">
+                    <a href="https://ok.ru/profile/588834059008" target="_blank">
+                        <svg class="bi me-2" width="32" height="32"><use xlink:href="#ok"/></svg>
+                    </a>
                 </div>
             </div>
         </div>
-        </div>
-    </footer>
+    </div>
+</div>
+</div>
+</footer>
 </main>
 @livewireScripts
 
@@ -473,7 +471,7 @@
         }, 500)
     })
 
-	
+
 
     document.addEventListener("DOMContentLoaded", () => {
         Livewire.hook('component.initialized', (component) => {})
